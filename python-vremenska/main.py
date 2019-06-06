0cbabcbcfe739d9f01d1e754a91de518c804c462
GoranOr <goran.orsolic19@gmail.com>
2019-06-06 16:53:20 +0200

    Add files via upload

---
+++ python-vremenska/main.py
@@ -0,0 +1,75 @@
+# glavni program
+
+import requests
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+
+    grad = "Zagreb"
+
+    unit = "metric"
+
+    apikey = "90bb55471680efa2ca732bd3975c2bc8"
+
+    url = "https://api.openweathermap.org/data/2.5/weather?q={0}&units={1}&appid={2}&lang=hr".format(grad, unit, apikey)
+
+    data = requests.get(url=url)
+
+    return render_template("index.html", data=data.json())
+
+
+@app.route("/split", methods=["GET"])
+def split():
+
+    grad = "Split"
+
+    unit = "metric"
+
+    apikey = "90bb55471680efa2ca732bd3975c2bc8"
+
+    url = "https://api.openweathermap.org/data/2.5/weather?q={0}&units={1}&appid={2}&lang=hr".format(grad, unit, apikey)
+
+    data = requests.get(url=url)
+
+    return render_template("split.html", data=data.json())
+
+@app.route("/rijeka", methods=["GET"])
+def rijeka():
+
+    grad = "Rijeka"
+
+    unit = "metric"
+
+    apikey = "90bb55471680efa2ca732bd3975c2bc8"
+
+    url = "https://api.openweathermap.org/data/2.5/weather?q={0}&units={1}&appid={2}&lang=hr".format(grad, unit, apikey)
+
+    data = requests.get(url=url)
+
+    return render_template("rijeka.html", data=data.json())
+
+@app.route("/osijek", methods=["GET"])
+def osijek():
+
+    grad = "Osijek"
+
+    unit = "metric"
+
+    apikey = "90bb55471680efa2ca732bd3975c2bc8"
+
+    url = "https://api.openweathermap.org/data/2.5/weather?q={0}&units={1}&appid={2}&lang=hr".format(grad, unit, apikey)
+
+    data = requests.get(url=url)
+
+    return render_template("osijek.html", data=data.json())
+
+if __name__== "__main__":
+    app.run()
+
+
+
+
